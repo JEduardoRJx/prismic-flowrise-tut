721a542362cfa88c9353de4919b73f390e8f54c9
eduardo <jeduardorjx@gmail.com>
2024-11-12 11:27:40 -0700

    feat: create hero slice and helper components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import { Metadata } from "next";
 import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
-// import { components } from "@/slices";
+import { components } from "@/slices";
 
 export default async function Page() {
   const client = createClient();
   const page = await client.getSingle("homepage");
-
-  return <div>Homepage!</div>
 
   return <SliceZone slices={page.data.slices} components={components} />;
 }
